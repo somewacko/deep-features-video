145b740e312568d09fbe37080204414e86b4b1bd
zo7 <zo7.flynn@gmail.com>
2016-07-30 10:54:14 -0400

    Add Keras VGG-19 model

---
+++ vggnet.py
@@ -0,0 +1,104 @@
+"""
+vggnet.py
+
+Module to load pre-trained VGG models in Keras.
+
+"""
+
+from __future__ import print_function
+
+import os
+import urllib
+
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+
+
+B_MEAN = 103.939
+G_MEAN = 116.779
+R_MEAN = 123.680
+
+IMAGE_SIZE = (224,224)
+
+VGG_19_WEIGHTS_URL = 'https://drive.google.com/file/d/0Bz7KyqmuGsilZ2RVeVhKY0FyRmc/view?usp=sharing'
+
+
+def VGG_19(weights_path):
+    """
+    Build a pre-trained VGG-19 model.
+
+    Args:
+        weights_path (str): Path to weights file. If no file exists, the file
+            will be downloaded instead.
+    Returns:
+        keras.model, VGG-19.
+    """
+
+    if not os.path.exists(weights_path):
+        print("No file found at {}. Downloading...".format(weights_path))
+        urllib.urlretrieve(VGG_19_WEIGHTS_URL, weights_path)
+
+    if not os.path.isfile(weights_path):
+        raise RuntimeError("Expected '{}' to be a file.".format(weights_path))
+
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))
+
+    model.load_weights(weights_path)
+
+    model.layers.pop()
+    model.layers.pop()
+
+    model.compile(optimizer='sgd', loss='mse')
+
+    return model
+
+
